6f8b17bcf622be3f277aabcab481b003c97326ff
Shevchenko Sergey <persalius@gmail.com>
2025-10-27 11:28:08 +0200

    asd

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    ignores: ["dist/", "node_modules/", "*.config.js"],
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      ecmaVersion: 2022,
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+      indent: ["error", 2],
+    },
+  },
+];
